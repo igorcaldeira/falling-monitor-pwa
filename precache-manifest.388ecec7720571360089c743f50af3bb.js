1efee79d03d3436e686c9dbe99ec427f9e7e1875
igorcaldeira <igorcaldeiraa@gmail.com>
2019-05-26 22:07:03 -0300

    Build on root for deploy GH pages

---
+++ precache-manifest.388ecec7720571360089c743f50af3bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ec510f33ae696f3d44911f1d6b46e74",
+    "url": "/falling-monitor-pwa/index.html"
+  },
+  {
+    "revision": "7a45a198ff587edd8daf",
+    "url": "/falling-monitor-pwa/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/falling-monitor-pwa/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "7a45a198ff587edd8daf",
+    "url": "/falling-monitor-pwa/static/js/main.eab64c7a.chunk.js"
+  },
+  {
+    "revision": "424f64ab828ef41fdee3",
+    "url": "/falling-monitor-pwa/static/js/runtime~main.36b2af9c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/falling-monitor-pwa/static/media/logo.5d5d9eef.svg"
+  }
+]);
